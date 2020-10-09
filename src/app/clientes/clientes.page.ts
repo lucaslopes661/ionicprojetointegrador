ad68ecd50a9bb8a03ee8caeba5703512ef2ab2a3
lucaslopes661 <lucaslopesgrisolia@gmail.com>
2020-10-09 19:26:12 -0300

    Aula Lista Cliente

--- src/app/clientes/clientes.page.ts
+++ src/app/clientes/clientes.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../model/cliente';
+import { ClienteService } from '../services/cliente.service';
 
 @Component({
   selector: 'app-clientes',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientesPage implements OnInit {
 
-  constructor() { }
+  lista : Cliente[] = [];
+
+  constructor( private clienteServ : ClienteService ) { }
 
   ngOnInit() {
+    this.clienteServ.listaDeClientes().subscribe(response=>{
+      //o servidor respondeu
+      console.log(response);
+      this.lista = response;
+      console.log(this.lista)
+    },err=>{
+      //erro
+    })
   }
 
 }
